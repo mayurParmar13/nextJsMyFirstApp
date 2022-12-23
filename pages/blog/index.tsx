e6d4a0223c00650f6311049ab337afc6b8a11bc3
Mayur Parmar <m.parmar@edexa.team>
2022-12-23 16:35:36 +0530

    feat: Link Component

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div>Blog
+        <div>
+            <Link href={"/blog/1"}>Blog 1</Link>
+            <Link href={"/blog/2"}>Blog 2</Link>
+            <Link href={"/blog/3"}>Blog 3</Link>
+            <Link href={"/blog/4"}>Blog 4</Link>
+        </div>
+    </div>
   )
 }
 
