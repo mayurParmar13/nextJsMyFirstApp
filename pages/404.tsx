cc2ed6bc90ed4916da76432f29c30ab4b350bfff
Mayur Parmar <m.parmar@edexa.team>
2022-12-23 16:49:25 +0530

    feat: 404 page not found add page in page folder

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1>PageNotFound</h1>
+      <Link href="/">Back to Home</Link>
+    </div>
+  );
+};
+
+export default PageNotFound;
