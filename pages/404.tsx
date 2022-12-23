d869bda182aed8ed1f24a3f4019302cf19b761ea
Mayur Parmar <m.parmar@edexa.team>
2022-12-23 18:36:46 +0530

    feat: routes.push method add

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 const PageNotFound = () => {
+    const routes = useRouter()
   return (
     <div>
       <h1>PageNotFound</h1>
       <Link href="/">Back to Home</Link>
+      <span style={{marginLeft: "10px", cursor:"pointer", color:"blue"}} onClick={() => routes.push("/blog")}>Back to Blog page</span>
     </div>
   );
 };
